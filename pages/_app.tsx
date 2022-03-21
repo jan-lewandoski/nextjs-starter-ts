66469ebd88252f7d05eace4257770fb0a96b8edb
Jan Lewandoski <jan.lewandoski@htdevelopers.com>
2022-03-21 15:12:54 +0100

    Add Nprogress package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import Router from 'next/router'
 import { appWithTranslation } from 'next-i18next'
 import { DefaultSeo } from 'next-seo'
 import SEO from '../next-seo.config'
 import React from 'react'
 import useProductsProvider, { ProductsContextType } from '@hooks/products/useProductsProvider'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+NProgress.configure({
+  showSpinner: false,
+  minimum: 0,
+  speed: 800,
+  easing: 'linear',
+})
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export const ProductsContext = React.createContext<ProductsContextType | null>(null)
 
