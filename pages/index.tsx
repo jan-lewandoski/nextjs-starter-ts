36a69aa11a6f2b4711111c65528833621fe54e83
Jan Lewandoski <jan.lewandoski@htdevelopers.com>
2022-04-15 19:21:16 +0200

    Add input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
+import { Input } from '@components/atomic-design/atoms/Input/Input'
 import Layout from '@components/Layout/Layout'
+import classNames from 'classnames'
 
 const Home = () => {
+  const className = undefined
   return (
     <Layout>
-      <div>
-        <h1>Home page</h1>
+      <div className="p-8">
+        <Input id="xaxa" label="Xaxa" name="xaxa" />
+        <h1 className={classNames(className, 'text-xl text-green-500', { 'font-bold': true })}>
+          Home page
+        </h1>
         <p className="text-lg">This is a home page</p>
       </div>
     </Layout>
