915d49b8a47c2688d5c1067be63dbed72c6e3206
Jan Lewandoski <jan.lewandoski@htdevelopers.com>
2022-04-24 19:57:49 +0200

    Add subscribing to the newsletter

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,49 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiHandler } from 'next'
 
-type Data = {
-  name: string
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', 'POST').status(405).json({ error: 'HTTP method should be POST' })
+    return
+  }
+
+  const MAILERLITE_GROUP_ID = process.env.MAILERLITE_GROUP_ID
+  const MAILERLITE_API_KEY = process.env.MAILERLITE_API_KEY
+
+  if (!MAILERLITE_API_KEY || !MAILERLITE_GROUP_ID) {
+    return res.status(500).json({
+      error: 'Env vars missing',
+    })
+  }
+
+  const email = req.body.email
+
+  if (typeof email !== 'string') {
+    res.status(400).json({})
+  }
+
+  const mailerliteResponse = await fetch(
+    `https://api.mailerlite.com/api/v2/groups/${MAILERLITE_GROUP_ID}/subscribers`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-MailerLite-ApiKey': MAILERLITE_API_KEY,
+      },
+      body: JSON.stringify({
+        email,
+      }),
+    },
+  )
+
+  if (!mailerliteResponse.ok) {
+    return res.status(500).json({
+      error: 'Error while subscribing to the newsletter',
+    })
+  }
+
+  return res.status(201).json({ message: `Successfully subscribed ${email} to the newsletter` })
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default handler
